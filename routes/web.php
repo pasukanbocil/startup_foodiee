4d7fa44a9e89765f300fd8d90ca77f5dd380553a
pasukanbocil <ctian8787@gmail.com>
2023-10-15 12:12:32 +0700

    saya ambil buff merah dan biru

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        'title' => 'Startup Foodiee'
+        'title' => 'Startup Foodiee - Home'
     ]);
 });
