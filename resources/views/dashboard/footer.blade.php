f7026a5497ce460c38ea16c6492bc7ff45546da5
pasukanbocil <ctian8787@gmail.com>
2023-10-24 16:01:28 +0700

    chore: :fire: bebep melmel

--- resources/views/dashboard/footer.blade.php
+++ resources/views/dashboard/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer-left">
-    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Dicky Satria Putra Herlambang</a>
+    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://www.instagram.com/dickysatriaph_">Dicky Satria Putra Herlambang</a>
 </div>
 <div class="footer-right">
 
